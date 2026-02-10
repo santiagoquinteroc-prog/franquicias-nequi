896b2efba4a06ffc8e3caa403f22252b77751ac6
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-02-09 21:23:33 -0500

    add initial project structure scaffold with Spring Boot, CORS configuration, and basic routing

---
+++ applications/app-service/src/main/java/com/red/franquicias/nequi/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package com.red.franquicias.nequi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "com.red.franquicias.nequi.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
