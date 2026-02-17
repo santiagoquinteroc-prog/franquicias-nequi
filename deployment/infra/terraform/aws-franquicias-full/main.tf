2662f30ffa155ea379446d27fb23594f5efe196a
Santiago Quintero <santiago.quinteroc@pragma.com.co>
2026-02-17 07:57:03 -0500

    feat(ecr): enable force delete for ECR repository to allow resource cleanup

--- deployment/infra/terraform/aws-franquicias-full/main.tf
+++ deployment/infra/terraform/aws-franquicias-full/main.tf
@@ -124,6 +124,7 @@ resource "aws_db_instance" "main" {
 resource "aws_ecr_repository" "app" {
   name                 = var.project_name
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
